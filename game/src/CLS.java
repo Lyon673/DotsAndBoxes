fba2c4427e8231df5dd02b8187c894cbc1a95ee1
lyonljs <lyon673forljs@gmail.com>
2024-04-15 16:27:19 +0800

    clear screen

--- game/src/CLS.java
+++ game/src/CLS.java
@@ -2,6 +2,8 @@ package src;
 
 import java.io.IOException;
 
+/** this class is designed to clear screen whenever the board changes*/
+
 public class CLS {
     public static void cls(){
         try {
